98fe95341f62714195c3d3a7df50d00ad165e91f
ZoRex15 <shpakstas007@gmail.com>
2023-05-21 14:57:40 +0300

    добавлена проверка под запрос билета в виде аудио файла

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,6 +1,6 @@
 from aiogram import Router, types
 from aiogram.types import FSInputFile
-from path import ticket_bel_dict, ticket_dict
+from path import ticket_bel_dict, ticket_dict, voise_ticket
 
 
 router: Router = Router()
@@ -9,10 +9,12 @@ router: Router = Router()
 
 @router.message()
 async def ticket(message: types.Message):
-    if str(message.text).lower() in ticket_dict:
-        media = FSInputFile(path=ticket_dict[message.text.lower()])
+    if message.text.lower() in ticket_dict:
+        media =  FSInputFile(path=ticket_dict[message.text.lower()])
         await message.answer_document(document=media)
-    else:
-        if str(message.text).lower() in ticket_bel_dict:
-            media = FSInputFile(path=ticket_bel_dict[message.text.lower()])
-            await message.answer_document(document=media)
+    if message.text.lower() in ticket_bel_dict:
+        media =  FSInputFile(path=ticket_bel_dict[message.text.lower()])
+        await message.answer_document(document=media)
+    if message.text.lower() in voise_ticket:
+        media = FSInputFile(path=voise_ticket[message.text.lower()])
+        await message.answer_audio(audio=media)
