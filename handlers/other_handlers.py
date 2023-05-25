a6e185ece6ffe39bef5f620b90e407cfe239645f
ZoRex15 <shpakstas007@gmail.com>
2023-05-25 20:39:07 +0300

    исправлен баг

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -10,15 +10,21 @@ router: Router = Router()
 @router.message()
 async def ticket(message: types.Message):
     if message.text.lower() in ticket_dict:
-        media =  FSInputFile(path=ticket_dict[message.text.lower()])
-        media_1 = FSInputFile(path=ticket_2_path[message.text.lower()])
-        await message.answer_document(document=media)
-        await message.answer_document(document=media_1)
+        try:
+            media =  FSInputFile(path=ticket_dict[message.text.lower()])
+            media_1 = FSInputFile(path=ticket_2_path[message.text.lower()])
+            await message.answer_document(document=media)
+            await message.answer_document(document=media_1)
+        except:
+            await message.answer_document(document=media)
     if message.text.lower() in ticket_bel_dict:
-        media =  FSInputFile(path=ticket_bel_dict[message.text.lower()])
-        media_2 = FSInputFile(path=ticket_2_path_bel[message.text.lower()])
-        await message.answer_document(document=media)
-        await message.answer_document(document=media_2)
+        try:
+            media =  FSInputFile(path=ticket_bel_dict[message.text.lower()])
+            media_2 = FSInputFile(path=ticket_2_path_bel[message.text.lower()])
+            await message.answer_document(document=media)
+            await message.answer_document(document=media_2)
+        except:
+            await message.answer_document(document=media)
     if message.text.lower() in voise_ticket:
         media = FSInputFile(path=voise_ticket[message.text.lower()])
         await message.answer_audio(audio=media)
